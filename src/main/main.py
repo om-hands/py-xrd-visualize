8a0d8ebb9ad349bf768b1aab277bb3501c3c7793
om-hands <okumuramasaki.hands@gmail.com>
2023-05-31 02:10:17 +0900

    split to directory

--- src/main/main.py
+++ src/main/main.py
@@ -1,59 +1,12 @@
-import traceback
-import sys
-from typing import Tuple, List, Literal
-
 import numpy as np
-import matplotlib.pyplot as plt
-import xrd_xy_parser.xy as xy
 
-
-def read_xy(target_filename: str) -> Tuple[np.ndarray, np.ndarray]:
-    try:
-        _, body, _ = xy.readstr(target_filename)
-        return body[:, 0], body[:, 1]
-    except xy.ParseError as e:
-        traceback.print_exception(e)
-        sys.exit(1)
-
-
-def read_file_dummy(target_filename: str) -> Tuple[np.ndarray, np.ndarray]:
-
-    x = np.linspace(0, 2 * np.pi)
-    y = np.sin(x)
-    return x, y
-
-
-def arrange_row(
-    xys: List[Tuple[np.ndarray, np.ndarray]],
-    range: List[float],
-    xlabel: str,
-    ylabel: str,
-    sharex: bool = True,
-    yscale: Literal["linear", "log", "symlog", "logit"] = "symlog",
-):
-
-    fig, axs = plt.subplots(nrows=len(xys), sharex=sharex)
-
-    if isinstance(axs, np.ndarray):
-        for ax, xy in zip(axs, xys):  # type: ignore
-            ax.plot(*xy)  # set data
-    else:
-        axs.plot(*xys[0])  # set data
-
-    plt.yscale(yscale)
-    plt.xlim(range)
-
-    fig.supxlabel(xlabel)
-    fig.supylabel(ylabel)
-
-    plt.grid(False)
-    plt.show()
+from py_xrd_visualize import visualize
 
 
 def main():
     target_files: list[str] = ["", ""]
-    xys = list(map(read_file_dummy, target_files))
-    arrange_row(
+    xys = list(map(visualize.read_file_dummy, target_files))
+    visualize.arrange_row(
         xys=xys,
         range=[0.0, 2 * np.pi],
         sharex=True,
---
+++ src/py_xrd_visualize/visualize.py
@@ -0,0 +1,50 @@
+import traceback
+import sys
+from typing import Tuple, List, Literal
+
+import numpy as np
+import matplotlib.pyplot as plt
+import xrd_xy_parser.xy as xy
+
+
+def read_xy(target_filename: str) -> Tuple[np.ndarray, np.ndarray]:
+    try:
+        _, body, _ = xy.readstr(target_filename)
+        return body[:, 0], body[:, 1]
+    except xy.ParseError as e:
+        traceback.print_exception(e)
+        sys.exit(1)
+
+
+def read_file_dummy(target_filename: str) -> Tuple[np.ndarray, np.ndarray]:
+
+    x = np.linspace(0, 2 * np.pi)
+    y = np.sin(x)
+    return x, y
+
+
+def arrange_row(
+    xys: List[Tuple[np.ndarray, np.ndarray]],
+    range: List[float],
+    xlabel: str,
+    ylabel: str,
+    sharex: bool = True,
+    yscale: Literal["linear", "log", "symlog", "logit"] = "symlog",
+):
+
+    fig, axs = plt.subplots(nrows=len(xys), sharex=sharex)
+
+    if isinstance(axs, np.ndarray):
+        for ax, xy in zip(axs, xys):  # type: ignore
+            ax.plot(*xy)  # set data
+    else:
+        axs.plot(*xys[0])  # set data
+
+    plt.yscale(yscale)
+    plt.xlim(range)
+
+    fig.supxlabel(xlabel)
+    fig.supylabel(ylabel)
+
+    plt.grid(False)
+    plt.show()

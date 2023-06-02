dd8cdfc485bd326075ce0f4d84e65188d279ced0
om-hands <okumuramasaki.hands@gmail.com>
2023-06-02 23:57:42 +0900

    add and add testcase

--- src/main/main.py
+++ src/main/main.py
@@ -4,16 +4,7 @@ from src.visualize import visualize
 
 
 def main():
-    target_files: list[str] = ["", ""]
-    xys = list(map(visualize.read_file_dummy, target_files))
-    visualize.arrange_row(
-        xys=xys,
-        range=[0.0, 2 * np.pi],
-        sharex=True,
-        xlabel=r"$2\theta[°]$",
-        ylabel=r"$Intensity[arb. unit]$",
-        yscale="linear",
-    )
+    pass
 
 
 if __name__ == "__main__":
--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,20 +1,65 @@
-import unittest
+import traceback
+import numpy as np
 from src.visualize import visualize
+from typing import Callable, Tuple, List, Literal
 
 
-class Test_visualize(unittest.TestCase):
-    def test_arrange_row(self):
-        target_files: list[str] = ["src/test/test.xy"]
+def Test_arrange_dummy():
+    xys = list(map(visualize.read_file_dummy, ["dummysine"]))
+    visualize.arrange_row(
+        xys=xys,
+        range=[0.0, 2 * np.pi],
+        sharex=True,
+        xlabel=r"$2\theta[°]$",
+        ylabel=r"$Intensity[arb. unit]$",
+        yscale="linear",
+    )
 
-        xys = list(map(visualize.read_xy, target_files))
-        visualize.arrange_row(
-            xys=xys,
-            range=[38.2, 39],
-            sharex=True,
-            xlabel=r"$2\theta[°]$",
-            ylabel=r"$Intensity[arb. unit]$",
-            yscale="log",
-        )
+
+def test_arrange_dummy_nth(n: int):
+    xys = list(map(visualize.read_file_dummy, ["dummysine"] * n))
+    visualize.arrange_row(
+        xys=xys,
+        range=[0.0, 2 * np.pi],
+        sharex=True,
+        xlabel=r"$2\theta[°]$",
+        ylabel=r"$Intensity[arb. unit]$",
+        yscale="linear",
+    )
+
+
+def Test_arrange_dummy_nth():
+    test_arrange_dummy_nth(5)
+
+
+def Test_arrange_rawdata():
+
+    xys = list(map(visualize.read_xy, ["src/test/test.xy"]))
+    visualize.arrange_row(
+        xys=xys,
+        range=[38.2, 39],
+        sharex=True,
+        xlabel=r"$2\theta[°]$",
+        ylabel=r"$Intensity[arb. unit]$",
+        yscale="log",
+    )
+
+
+def test_arrange_rawdata_nth(n: int):
+
+    xys = list(map(visualize.read_xy, ["src/test/test.xy"] * n))
+    visualize.arrange_row(
+        xys=xys,
+        range=[38.2, 39],
+        sharex=True,
+        xlabel=r"$2\theta[°]$",
+        ylabel=r"$Intensity[arb. unit]$",
+        yscale="log",
+    )
+
+
+def Test_arrange_rawdata_nth():
+    test_arrange_rawdata_nth(5)
 
 
 def main():

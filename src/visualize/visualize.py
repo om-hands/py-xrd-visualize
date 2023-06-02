188032773068c3a1966d98bec058081ac65b989e
om-hands <okumuramasaki.hands@gmail.com>
2023-06-03 00:32:02 +0900

    add document

--- src/visualize/visualize.py
+++ src/visualize/visualize.py
@@ -9,6 +9,21 @@ import xrd_xy_parser.xy as xy
 
 
 def read_xy(target_filename: str) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    read file from `target_filename` ,and return x-y data.
+    Parameters
+    ---
+    target_filename:xy-styled file name.
+
+    Return
+    ---
+    x,y:
+        x,y:np.ndarray
+
+    Error
+    ---
+       If file not found, exit program.
+    """
     try:
         _, body, _ = xy.readstr(target_filename)
         return body[:, 0], body[:, 1]
@@ -18,7 +33,17 @@ def read_xy(target_filename: str) -> Tuple[np.ndarray, np.ndarray]:
 
 
 def read_file_dummy(target_filename: str) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Parameters
+    ---
+    target_filename:not used.
 
+    Return
+    ---
+    x,y:
+        x:np.ndarray. 0~2π
+        y:np.ndarray. sin(x)
+    """
     x = np.linspace(0, 2 * np.pi)
     y = np.sin(x)
     return x, y
@@ -33,6 +58,21 @@ def arrange_row(
     yscale: Literal["linear", "log", "symlog", "logit"] = "symlog",
 ):
 
+    """
+    `arrange_row` displays a graph of the list of data `xys`.
+
+    Parameters:
+    ---
+        `xys`:xy-styled input data.
+
+        `range`:x-axis range.
+
+        `xlabel`,`ylabel`:label.
+
+        `sharex`:is share x-axis.
+
+        `yscale`:y-axis scale.
+    """
     fig, axs = plt.subplots(nrows=len(xys), sharex=sharex, squeeze=False)
 
     # print(type(axs), axs[0], axs[0])

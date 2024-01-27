12dc9be3cce7bef54f4e81630c903ec2d38a3861
om-hands <okumuramasaki.hands@gmail.com>
2024-01-28 03:53:03 +0900

    update test

--- src/test/test_xrd.py
+++ src/test/test_xrd.py
@@ -3,20 +3,26 @@ from pathlib import Path
 import matplotlib as mpl
 
 import matplotlib.pyplot as plt
+from py_xrd_visualize.XYs import read_xys
 from py_xrd_visualize.util import Gauss, Voigt
 
 from py_xrd_visualize.visualize import (
     Annotater,
     ax_func_horizontal_annotates,
+    ax_legends,
+    ax_plots,
+    complete_ax,
+    complete_fig,
     fig_conf_show,
 )
 
 from py_xrd_visualize.xrd import (
-    fig_2θ_ω_1axis,
+    ax_2θ_ω,
+    ax_φ,
+    ax_ω,
     fig_any_scan_1axis,
     fig_any_scan_naxis,
-    fig_φ_scan_1axis,
-    fig_ω_scan_1axis,
+    make_fig_1axis,
 )
 
 
@@ -26,23 +32,28 @@ def plot_2θ_ω_1axis():
     )
 
     print(paths)
-    fig = fig_2θ_ω_1axis(
-        paths,
-        [1, 1, 1],
-        legends=["test1", "test2", "test3"],
-        legend_title="2th omega",
-        legend_reverse=True,
-        range_=(10, 70),
-        # range_=(38.2, 39.0),
-        ax_func=ax_func_horizontal_annotates(
-            10,
-            [
-                Annotater(38.5, 0, "38.5,10-5", (0.0, -5)),
-                Annotater(38.7, 0, "aaaa"),
+    make_fig_1axis(
+        ax_2θ_ω(
+            paths,
+            [1, 1],
+            legends=["test1", "test2"],
+            legend_title="2th omega",
+            legend_reverse=True,
+            # range_=(10, 70),
+            # range_=(38.2, 39.0),
+            ax_funcs=[
+                ax_func_horizontal_annotates(
+                    10,
+                    [
+                        Annotater(38.5, 0, "38.5,10-5", (0.0, -5)),
+                        Annotater(38.7, 0, "aaaa"),
+                    ],
+                )
             ],
         ),
-        fig_conf=fig_conf_show(figratio=(5, 6)),
+        fig_funcs=[fig_conf_show(figratio=(5, 6))],
     )
+
     plt.show()
     # fig.savefig()
 
@@ -61,36 +72,30 @@ def plot_ω_scan_1axis():
 
     print(paths)
 
-    fig = fig_ω_scan_1axis(
-        paths=paths,
-        amps=[1400, 35],
-        # legends=["test1", "test2noisy"],
-        range_=(-4, 4),
-        # ax_legends=ax_default_legends(["test1", "test2"]),
-        optimizer=Gauss(),
-        fig_conf=fig_conf_show(figratio=(10, 5)),
-        show_optparam=True,
+    make_fig_1axis(
+        ax_func=ax_ω(
+            paths=paths,
+            amps=[1400, 35],
+            legends=["test1", "test2noisy"],
+            range_=(-4, 4),
+            # ax_legends=ax_default_legends(["test1", "test2"]),
+            optimizer=Gauss(),
+            show_optparam=True,
+        ),
+        fig_funcs=[fig_conf_show(figratio=(10, 5))],
     )
-    fig_ω_scan_1axis(
-        paths=paths,
-        amps=[1400, 35],
-        # legends=["test1", "test2noisy"],
-        range_=(-4, 4),
-        # ax_legends=ax_default_legends(["test1", "test2"]),
-        optimizer=Voigt(),
-        fig_conf=fig_conf_show(figratio=(10, 5)),
-        show_optparam=True,
-    )
-    plt.show()
 
-    fig_ω_scan_1axis(
-        paths=paths,
-        amps=[1400, 35],
-        # legends=["test1", "test2noisy"],
-        range_=(-4, 4),
-        # ax_legends=ax_default_legends(["test1", "test2"]),
-        # optimize_func=util.gauss(),
-        show_optparam=False,
+    make_fig_1axis(
+        ax_func=ax_ω(
+            paths=paths,
+            amps=[1400, 35],
+            legends=["test1", "test2noisy"],
+            range_=(-4, 4),
+            # ax_legends=ax_default_legends(["test1", "test2"]),
+            optimizer=Voigt(),
+            show_optparam=True,
+        ),
+        fig_funcs=[fig_conf_show(figratio=(10, 5))],
     )
     plt.show()
 
@@ -104,32 +109,47 @@ def plot_φ_scan_1axis():
 
     print(paths)
 
-    fig_φ_scan_1axis(
-        paths=paths,
-        scantimes_sec=[1, 1],
-        # legends=["1", ""],
-        range_=(0.2, 0.8),
-        fig_conf=fig_conf_show(),
-        roll_x_deg=0.0,
+    # fig_φ_scan_1axis(
+    #     paths=paths,
+    #     scantimes_sec=[1, 1],
+    #     legends=["1", "2"],
+    #     range_=(0.2, 0.8),
+    #     fig_conf=fig_conf_show(),
+    #     roll_x_deg=0.0,
+    # )
+    make_fig_1axis(
+        ax_func=ax_φ(
+            paths=paths,
+            scantimes_sec=[1, 1],
+            legends=["1", "2"],
+            range_=(0.2, 0.8),
+            roll_x_deg=0.0,
+        ),
+        fig_funcs=[],
     )
     # plt.show()
 
-    fig_φ_scan_1axis(
-        paths=paths,
-        scantimes_sec=[1, 1],
-        # legends=["2", ""],
-        range_=(0.3, 0.8),
-        fig_conf=fig_conf_show(),
-        roll_x_deg=0.304,
+    make_fig_1axis(
+        ax_func=ax_φ(
+            paths=paths,
+            scantimes_sec=[1, 1],
+            range_=(0.3, 0.8),
+            roll_x_deg=0.304,
+        ),
+        fig_funcs=[],
     )
-    fig_φ_scan_1axis(
-        paths=paths,
-        scantimes_sec=[1, 1],
-        # legends=["2", ""],
-        range_=(0.3, 0.8),
-        fig_conf=fig_conf_show(),
-        roll_x_deg=0.311,
+
+    make_fig_1axis(
+        ax_func=ax_φ(
+            paths=paths,
+            scantimes_sec=[1, 1],
+            # legends=["2", ""],
+            range_=(0.3, 0.8),
+            roll_x_deg=0.311,
+        ),
+        fig_funcs=[],
     )
+
     plt.show()
     # fig.savefig()
 
@@ -149,6 +169,9 @@ def plot_any_scan_1axis():
     with mpl.rc_context({"font.size": 10}):
         fig_any_scan_1axis(paths)
         fig_any_scan_naxis(paths)
+        plt.show()
+
+
 def complete_test():
     paths: list[TextIOBase | str | Path] = list(
         map(lambda x: Path("src/test/") / x, ["test.xy"] * 2)
@@ -172,7 +195,7 @@ def complete_test():
                 ax_legends=ax_legends(legends=["test2"]),
             ),
         ],
-        fig_func=[fig_conf_show(figratio=(5, 6))],
+        fig_funcs=[fig_conf_show(figratio=(5, 6))],
     )
     cfig(fig)
     plt.show()
@@ -192,4 +215,8 @@ if __name__ == "__main__":
             "font.size": 18,
         }
     )
+    plot_2θ_ω_1axis()
+    plot_ω_scan_1axis()
+    plot_φ_scan_1axis()
+    plot_any_scan_1axis()
     complete_test()

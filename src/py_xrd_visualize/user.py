a71c3b35db1613915f1673622ae5644dc3e76c02
om-hands <okumuramasaki.hands@gmail.com>
2024-01-28 05:04:33 +0900

    add example

--- src/py_xrd_visualize/user.py
+++ src/py_xrd_visualize/user.py
@@ -32,3 +32,32 @@ class Scan:
     path: TextIOBase | str | Path
     legend: str
     scantime_s: float
+
+
+def example():
+    ls = [
+        Scan(
+            path=Path("src/test/test.xy"),
+            legend="test1",
+            scantime_s=1.0,
+        ),
+        Scan(
+            path=Path("src/test/test.xy"),
+            legend="test2",
+            scantime_s=2.0,
+        ),
+        Scan(
+            path=Path("src/test/test.xy"),
+            legend="test3",
+            scantime_s=3.0,
+        ),
+    ]
+    paths = [l.path for l in ls]
+    legends = [l.legend for l in ls]
+    scantimes = [l.scantime_s for l in ls]
+
+    @dataclass
+    class AddedScan(Scan):
+        color: str
+
+    AddedScan("src/test/test.xy", "test1", 1.0, "red")
